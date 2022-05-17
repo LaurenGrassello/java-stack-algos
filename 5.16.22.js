7dd7b0c491458f6125d56962b92ed65532d777be
LaurenOdalen <laurenodalen@gmail.com>
2022-05-17 09:48:32 -0600

    commit

--- 5.16.22.js
+++ 5.16.22.js
@@ -3,9 +3,9 @@
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 
+
 var nums = [2,7,11,15]
 var target = 9
-
 for(var i = 0; i <nums.length; i++){
     if(nums[i] + nums[i + 1] == target){
         console.log(i, i+1)
